76d4545a8dbae6dabfec58b87c6c748c8df73bd3
shixuewen <friendlysxw@163.com>
2022-03-30 17:58:17 +0800

    feat: add button component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-03-30 03:44:57
- * @LastEditTime: 2022-03-30 04:15:02
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-30 14:26:56
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \sxw-react-components\src\index.tsx
  */
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,8 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+/*
+ * @Author: your name
+ * @Date: 2022-03-30 14:51:18
+ * @LastEditTime: 2022-03-30 16:29:12
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \sxw-react-components\src\reportWebVitals.ts
+ */
+import { ReportHandler } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
